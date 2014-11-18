18ead49f00c5115400e05b340f251522baeef3d2
Yu Li <YLiLarry@gmail.com>
2014-11-18 14:26:16 -0500

    view.h full interface

--- view/view.h
+++ view/view.h
@@ -5,27 +5,15 @@
     
 /*
     I just realized that since the GraphicView (if there's animation) should calls draw() on itself in a seperate thread 
-    with a fixed FPS, the TextView better calls draw() on itself too. That is, when you call `sawp()` on both View, 
+    with a fixed FPS, the TextView better calls draw() on itself too. That is, when you call `swap()` on both View, 
     they change the positions of cells and display automatically. Only the `init()` method should not call `draw()`.
 */
 
 #ifndef __VIEW__H__
 #define __VIEW__H__
 #include "../model/model.h"
-    
-    class CellView {
-        
-        public:
-            int x;
-            int y;
-            COLOUR colour;
-            virtual void moveUp() = 0;
-            virtual void moveDown() = 0;
-            virtual void moveLeft() = 0;
-            virtual void moveRight() = 0;
-            virtual void destory() = 0;
-            
-    };
+
+    class CellView;
     
     class View {
 
@@ -35,23 +23,24 @@
             unsigned int m_movesRemain;
             unsigned int m_level;
             unsigned int m_hiScore;
-            std::vector< std::vector<CellView> > board;
+            std::vector< std::vector<CellView*> > m_board;
             virtual void draw() const = 0;
             
         public:
-            View(Model*, int);
-            ~View();
-            virtual void init() = 0;
-            virtual void init(std::vector<Cell*>) = 0; // overloaded
+            View(Model*, int size);
+            virtual ~View();
+            virtual void changeColour(int,int,COLOUR) = 0;
+            virtual void changeType(int,int,CELLTYPE) = 0;
+            virtual void change(std::vector<Cell*>) = 0; // reqires the Cell class definition
             virtual void score() = 0;
             virtual void level() = 0;
             virtual void movesRemain() = 0;
             virtual void hiScore() = 0;
             
-            virtual void sawp(int,int,COLOUR) = 0;
-            virtual void clearMatched(int,int) = 0;
-            virtual void clearMatched(std::vector<Cell*>) = 0; // overloaded
-            
+            virtual void swap(int,int,DIRECTION) = 0;
+            virtual void destory(int,int) = 0;
+            virtual void destory(std::vector<Cell*>) = 0; // reqires the Cell class definition
+
     };
     
 #endif
