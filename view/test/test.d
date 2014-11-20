f234e733ad04487db317665a65177c565d7a2cc9
Yu Li <YLiLarry@gmail.com>
2014-11-20 08:56:18 -0500

    bugfix

--- view/test/test.d
+++
@@ -1,4 +0,0 @@
-view/test/test.o: view/test/test.cc view/test/../view.h \
- view/test/../viewabstract.h view/test/../../public/global.h \
- view/test/../cellviewabstract.h view/test/../textview/textview.h \
- view/test/../textview/../viewabstract.h
