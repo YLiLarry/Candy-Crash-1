ddbf1744f898d849c165d6049ba0ff8b45925204
wahidrahim <wahidrahim@gmail.com>
2014-11-30 21:45:44 -0500

    public square

--- model/square.h
+++ model/square.h
@@ -9,6 +9,8 @@ const int NEIGHBOURS = 4;
 
 class Square {
 
+	public:
+
 	View *view;
 	Square ***grid;
 	int gridSize;
@@ -25,7 +27,6 @@ class Square {
 
 	void notifyNeighbours();
 
-	public:
 
 	Square *neighbour[NEIGHBOURS];
 
